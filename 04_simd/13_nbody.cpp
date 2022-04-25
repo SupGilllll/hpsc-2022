353ce43d1a34dc3caae7f901aacfe28f2ec12aef
SupGilllll <zhao.x.af@m.titech.ac.jp>
2022-04-25 12:59:05 +0900

    Assignment2

--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -1,26 +1,39 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+#include <immintrin.h>
 
 int main() {
-  const int N = 8;
-  float x[N], y[N], m[N], fx[N], fy[N];
-  for(int i=0; i<N; i++) {
-    x[i] = drand48();
-    y[i] = drand48();
-    m[i] = drand48();
-    fx[i] = fy[i] = 0;
-  }
-  for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-      if(i != j) {
-        float rx = x[i] - x[j];
-        float ry = y[i] - y[j];
-        float r = std::sqrt(rx * rx + ry * ry);
-        fx[i] -= rx * m[j] / (r * r * r);
-        fy[i] -= ry * m[j] / (r * r * r);
-      }
+    const int N = 8;
+    float x[N], y[N], m[N], fx[N], fy[N];
+    for (int i = 0; i < N; i++) {
+        x[i] = drand48();
+        y[i] = drand48();
+        m[i] = drand48();
+        fx[i] = fy[i] = 0;
     }
-    printf("%d %g %g\n",i,fx[i],fy[i]);
-  }
+    for (int i = 0; i < N; i++) {
+        __m256 neg = _mm256_set1_ps(-1.f);
+        __m256 xvec_i = _mm256_set1_ps(x[i]);    __m256 xvec_j = _mm256_load_ps(x);
+        __m256 yvec_i = _mm256_set1_ps(y[i]);    __m256 yvec_j = _mm256_load_ps(y);
+        __m256 rvec_x = _mm256_sub_ps(xvec_i, xvec_j);    __m256 rvec_y = _mm256_sub_ps(yvec_i, yvec_j);
+        __m256 ones = _mm256_set1_ps(1.f);    __m256 zeros = _mm256_set1_ps(0.f);
+        __m256 mask = _mm256_cmp_ps(xvec_i, xvec_j, _CMP_NEQ_UQ);
+        __m256 r = _mm256_rsqrt_ps(_mm256_add_ps(_mm256_mul_ps(rvec_x, rvec_x), _mm256_mul_ps(rvec_y, rvec_y)));
+        r = _mm256_blendv_ps(zeros, r, mask);
+        __m256 mxvec = _mm256_load_ps(m);
+        __m256 fvec_x = _mm256_mul_ps(_mm256_mul_ps(_mm256_mul_ps(rvec_x, mxvec), _mm256_mul_ps(r, _mm256_mul_ps(r, r))), neg);
+        __m256 fvec_y = _mm256_mul_ps(_mm256_mul_ps(_mm256_mul_ps(rvec_y, mxvec), _mm256_mul_ps(r, _mm256_mul_ps(r, r))), neg);
+        __m256 x_sum = _mm256_permute2f128_ps(fvec_x, fvec_x, 1);
+        x_sum = _mm256_add_ps(x_sum, fvec_x);
+        x_sum = _mm256_hadd_ps(x_sum, x_sum);
+        x_sum = _mm256_hadd_ps(x_sum, x_sum);
+        __m256 y_sum = _mm256_permute2f128_ps(fvec_y, fvec_y, 1);
+        y_sum = _mm256_add_ps(y_sum, fvec_y);
+        y_sum = _mm256_hadd_ps(y_sum, y_sum);
+        y_sum = _mm256_hadd_ps(y_sum, y_sum);
+        fx[i] = _mm256_cvtss_f32(x_sum);
+        fy[i] = _mm256_cvtss_f32(y_sum);
+        printf("%d %g %g\n", i, fx[i], fy[i]);
+    }
 }
